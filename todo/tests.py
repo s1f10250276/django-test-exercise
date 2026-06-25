ae8b45fa3deb97703c1a9f4bb2889f82a0af1d57
1f10250276 <s1f102502766@iniad.org>
2026-06-25 13:46:52 +0900

    コードを整形

--- todo/tests.py
+++ todo/tests.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
+
 # Create your tests here.
 class SampleTestCase(TestCase):
     def test_sample1(self):
@@ -11,8 +12,8 @@ class SampleTestCase(TestCase):
 
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
-        due = timezone.make_aware(datetime(2024,6,30,23,59,59))
-        task = Task(title='task1', due_at = due)
+        due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
+        task = Task(title = 'task1', due_at = due)
         task.save()
 
         task = Task.objects.get(pk=task.pk)
@@ -30,24 +31,24 @@ class TaskModelTestCase(TestCase):
         self.assertEqual(task.due_at, None)
 
     def test_is_overdue_future(self):
-        due = timezone.make_aware(datetime(2024,6,30,23,59,59))
-        current = timezone.make_aware(datetime(2024,6,30,0,0,0))
-        task = Task(title='task1', due_at = due)
+        due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
+        current = timezone.make_aware(datetime(2024, 6, 30, 0, 0, 0))
+        task = Task(title = 'task1', due_at = due)
         task.save()
 
         self.assertFalse(task.is_overdue(current))
     
     def test_is_overdue_past(self):
-        due = timezone.make_aware(datetime(2024,6,30,23,59,59))
-        current = timezone.make_aware(datetime(2024,7,1,0,0,0))
-        task = Task(title='task2', due_at = due)
+        due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
+        current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
+        task = Task(title = 'task2', due_at = due)
         task.save()
 
         self.assertTrue(task.is_overdue(current))
     
     def test_is_overdue_none(self):
-        current = timezone.make_aware(datetime(2024,7,1,0,0,0))
-        task = Task(title='task3',)
+        current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
+        task = Task(title = 'task3',)
         task.save()
 
         self.assertFalse(task.is_overdue(current))
