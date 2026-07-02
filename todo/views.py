ac08d1111b55b917feb4dc69081f12e1b7fdf3b8
1f10250276 <s1f102502766@iniad.org>
2026-07-02 14:42:50 +0900

    ファイルを修正2

--- todo/views.py
+++ todo/views.py
@@ -34,16 +34,18 @@ def detail(request, task_id):
     }
     return render(request, 'todo/detail.html', context)
 
+
 def close(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
-    
+
     task.completed = True
     task.save()
 
     return redirect(index)
+
 
 def update(request, task_id):
     try:
