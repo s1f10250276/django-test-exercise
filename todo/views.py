e8d7541dca86e7857c592f72387c4c931fed0db5
1F10250239 <s1F102502395@iniad.org>
2026-07-02 15:12:56 +0900

    タスクの削除機能を追加

--- todo/views.py
+++ todo/views.py
@@ -50,3 +50,11 @@ def update(request, task_id):
         'task': task
     }
     return render(request, "todo/edit.html", context)
+
+def delete(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+    task.delete()
+    return redirect(index)
